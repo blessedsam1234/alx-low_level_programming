0c8199a5dd98aff4c2b84d774478a0ea3b78adb0
blessedsam1234 <ugochukwusamuelchiedozie@gmail.com>
2023-09-09 15:42:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,28 +3,24 @@
 /**
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
- */
-int main(void)
+ */int main(void)
 {
 int n, m, l;
 
-for (n = 48; n < 58; n++)
+for (n = 48; n < 56; n++)
 {
-for (m = 49; m < 58; m++)
+for (m = n + 1; m < 57; m++)
 {
-for (l = 50; l < 58; l++)
-{
-if (l > m && m > n)
+for (l = m + 1; l < 58; l++)
 {
 putchar(n);
 putchar(m);
 putchar(l);
 
-if (n != 55 || m != 56 || l != 57)
+if (n != 55)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
