11b10a8035e69561d776add86d5af27a8c76d451
blessedsam1234 <ugochukwusamuelchiedozie@gmail.com>
2023-09-09 14:46:19 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,17 +2,15 @@
 /**
  * main - prints numbers between 0 to 9 and letters between a to f
  * Return: Always 0.
- */
+*/
+
 int main(void)
 {
 int i;
-for (i = 48; i < 58; i++)
+for (i = 48; i < 103; i++)
 {
 putchar(i);
 }
-for (i = 97; i < 103; i++)
-{
 putchar('\n');
-}
 return (0);
 }
