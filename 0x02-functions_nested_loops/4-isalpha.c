64a7805981824e90dda2d73ba545ba24829cc8ec
blessedsam1234 <ugochukwusamuelchiedozie@gmail.com>
2023-09-12 19:59:49 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _is alpha - checks for alphabetic characters
+ * _isalpha - checks for alphabetic characters
  * @C: the characters to be checked
  * Return: 1 for alphabets and 0 for anything else
  */
