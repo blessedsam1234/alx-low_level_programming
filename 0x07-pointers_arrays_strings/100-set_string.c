568669b6cf4b9a9af7d2aec228cfd859f65f2ba2
blessedsam1234 <ugochukwusamuelchiedozie@gmail.com>
2023-09-25 15:55:25 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - main function
+ * @s: function parameter
+ * @to: function parameter
+ * Return: Always 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
