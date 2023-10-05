f880d17cf90e05cd9d0acd831ccef266796fb1fc
blessedsam1234 <ugochukwusamuelchiedozie@gmail.com>
2023-10-05 01:28:52 -0700

    creating my static library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic characters
+ * @C: the characters to be checked
+ * Return: 1 for alphabets and 0 for anything else
+ */
+int _isalpha(int C)
+{
+if ((C >= 65 && C <= 97) || (C >= 97 && C <= 122))
+{
+return (1);
+}
+return (0);
+}
